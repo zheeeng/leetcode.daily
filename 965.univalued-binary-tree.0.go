231f0216171f2c9f6664028178783373548bf2f2
Zheeeng <hi@zheeeng.me>
2019-03-30 15:52:14 +0800

    Solution for 965 by go

---
+++ 965.univalued-binary-tree.0.go
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=965 lang=golang
+ *
+ * [965] Univalued Binary Tree
+ *
+ * https://leetcode.com/problems/univalued-binary-tree/description/
+ *
+ * algorithms
+ * Easy (67.33%)
+ * Total Accepted:    24.4K
+ * Total Submissions: 36.2K
+ * Testcase Example:  '[1,1,1,1,1,null,1]'
+ *
+ * A binary tree is univalued if every node in the tree has the same value.
+ *
+ * Return true if and only if the given tree is univalued.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,1,1,1,1,null,1]
+ * Output: true
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,2,2,5,2]
+ * Output: false
+ *
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * The number of nodes in the given tree will be in the range [1, 100].
+ * Each node's value will be an integer in the range [0, 99].
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isUnivalTree(root *TreeNode) bool {
+	return isSameValue(root, root.Val)
+}
+
+func isSameValue(root *TreeNode, val int) bool {
+	if root == nil {
+		return true
+	}
+
+	return root.Val == val &&
+		isSameValue(root.Left, val) &&
+		isSameValue(root.Right, val)
+}
+
