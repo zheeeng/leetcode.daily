75b78d42f562697dca91b7870fd7ecf25714dc29
Zheeeng <hi@zheeeng.me>
2019-04-01 14:08:02 +0800

    704/go-0

---
+++ 704.binary-search.0.go
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=704 lang=golang
+ *
+ * [704] Binary Search
+ *
+ * https://leetcode.com/problems/binary-search/description/
+ *
+ * algorithms
+ * Easy (46.07%)
+ * Total Accepted:    39.4K
+ * Total Submissions: 85.2K
+ * Testcase Example:  '[-1,0,3,5,9,12]\n9'
+ *
+ * Given a sorted (in ascending order) integer array nums of n elements and a
+ * target value, write a function to search target in nums. If target exists,
+ * then return its index, otherwise return -1.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * You may assume that all elements in nums are unique.
+ * n will be in the range [1, 10000].
+ * The value of each element in nums will be in the range [-9999, 9999].
+ *
+ *
+ */
+func search(nums []int, target int) int {
+	minIndex := 0
+	maxIndex := len(nums) - 1
+	min := nums[minIndex]
+	max := nums[maxIndex]
+	index := -1
+	indexValue := 0
+
+	if target < min {
+		return -1
+	}
+
+	if target > max {
+		return -1
+	}
+
+	for min <= max {
+		if target == min {
+			return minIndex
+		}
+		if target == max {
+			return maxIndex
+		}
+		if min == max {
+			break
+		}
+
+		if nextIndex := (maxIndex-minIndex+1)*(target-min)/(max-min) + minIndex; nextIndex != index {
+			index = nextIndex
+		} else {
+			break
+		}
+
+		indexValue = nums[index]
+
+		if indexValue == target {
+			return index
+		} else if indexValue > target {
+			maxIndex = index - 1
+			max = nums[maxIndex]
+		} else {
+			minIndex = index + 1
+			min = nums[minIndex]
+		}
+	}
+
+	return -1
+}
+
