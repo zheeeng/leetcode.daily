b0282f89500b78890b8927e61d48ef7dcb303289
Zheeeng <hi@zheeeng.me>
2019-04-02 13:01:05 +0800

    26/go

---
+++ 26.remove-duplicates-from-sorted-array.0.[nopass].go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * algorithms
+ * Easy (39.98%)
+ * Total Accepted:    551.9K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that each
+ * element appear only once and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ *
+ * Example 1:
+ *
+ *
+ * Given nums = [1,1,2],
+ *
+ * Your function should return length = 2, with the first two elements of nums
+ * being 1 and 2 respectively.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ *
+ * Example 2:
+ *
+ *
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * Your function should return length = 5, with the first five elements of nums
+ * being modified to 0, 1, 2, 3, and 4 respectively.
+ *
+ * It doesn't matter what values are set beyond the returned length.
+ *
+ *
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference, which means
+ * modification to the input array will be known to the caller as well.
+ *
+ * Internally you can think of this:
+ *
+ *
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ *
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len
+ * elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ *
+ */
+
+// Note: This code doesn't pass the OJ, coz the OJ cares about the order, this code just removeDuplicates and cares about minimum value assigning.
+func removeDuplicates(nums []int) int {
+	l := len(nums)
+	m := make(map[int]struct{}, l)
+
+	for i := 0; i < l; i++ {
+		v := nums[i]
+		if _, ok := m[v]; ok {
+			for j := l - 1; j > i; j-- {
+				v2 := nums[j]
+				if _, ok = m[v2]; ok {
+					l--
+					continue
+				}
+				m[v2] = struct{}{}
+				nums[i] = v2
+
+				break
+			}
+
+			l--
+		}
+
+		m[v] = struct{}{}
+	}
+
+	return l
+}
+
