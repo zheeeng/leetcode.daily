335ab9ead47d5a937130a3d66291f184f12af70c
Zheeeng <hi@zheeeng.me>
2019-04-02 23:01:39 +0800

    70/go-2

---
+++ 70.climbing-stairs.2.go
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=70 lang=golang
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (43.76%)
+ * Total Accepted:    375.5K
+ * Total Submissions: 858.2K
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ */
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	list := make([]int, n)
+	list[0] = 1
+	list[1] = 2
+	for i := 2; i < n; i++ {
+		list[i] = list[i-1] + list[i-2]
+	}
+
+	return list[n-1]
+}
+
