9810bcbada1fdd70ea1abedf98e494f1dd2b62fc
Zheeeng <hi@zheeeng.me>
2019-04-04 09:50:30 +0800

    1/js

---
+++ 1.two-sum.0.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (42.90%)
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.8M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    let index2 = null
+    const index = nums.findIndex((num, i) => {
+      index2 = nums.slice(i + 1).findIndex(num2 => num2 === target - num)
+      return ~index2
+    })
+    return [index, index + index2 + 1]
+  }
