a8d190577563857439076932021c57616f8421e9
Zheeeng <hi@zheeeng.me>
2019-04-04 09:44:49 +0800

    53/js

---
+++ 53.maximum-subarray.0.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (43.11%)
+ * Total Accepted:    492.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = (nums) => {
+    let max = 0
+    let sum = 0
+    for (let num of nums) {
+        sum += num
+        if (sum < 0) sum = 0
+        if (sum > max) max = sum
+    }
+    if (max === 0) max = Math.max.apply(null, nums)
+    return max
+}
+
