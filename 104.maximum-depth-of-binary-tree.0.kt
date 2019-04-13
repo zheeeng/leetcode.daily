f740a2961efecd121bf98881ae9f18491586300a
Zheeeng <hi@zheeeng.me>
2019-04-13 17:07:22 +0800

    104/kt

---
+++ 104.maximum-depth-of-binary-tree.0.kt
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=104 lang=kotlin
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.74%)
+ * Total Accepted:    482.7K
+ * Total Submissions: 805.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * return its depth = 3.
+ *
+ */
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution_maxDepth_0 {
+    fun maxDepth(root: TreeNode?): Int = when {
+        root == null -> 0
+        root.left == null && root.right == null -> 1
+        else -> 1 + maxOf(maxDepth(root.left), maxDepth(root.right))
+    }
+}
