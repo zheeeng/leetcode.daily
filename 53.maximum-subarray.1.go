7a6359323440545d53122f60a23f40c4588ea63e
Zheeeng <hi@zheeeng.me>
2019-04-17 21:43:16 +0800

    53/go-1

---
+++ 53.maximum-subarray.1.go
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=53 lang=golang
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (43.06%)
+ * Total Accepted:    490.1K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ *
+ * Follow up:
+ *
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ *
+ */
+import (
+	"sort"
+)
+
+func maxSubArray(nums []int) int {
+	max := 0
+	sum := 0
+	i := 0
+	l := len(nums)
+	for ; i < l; i++ {
+		sum += nums[i]
+		if sum < 0 {
+			sum = 0
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+
+	if max == 0 {
+		sort.Ints(nums)
+		return nums[len(nums)-1]
+	}
+
+	return max
+}
