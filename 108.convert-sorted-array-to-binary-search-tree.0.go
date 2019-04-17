3c9654880018dda3358cce1939d74bf3f34a2c3f
Zheeeng <hi@zheeeng.me>
2019-04-17 21:16:48 +0800

    108/go

---
+++ 108.convert-sorted-array-to-binary-search-tree.0.go
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=108 lang=golang
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (49.81%)
+ * Total Accepted:    251.3K
+ * Total Submissions: 501.9K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given an array where elements are sorted in ascending order, convert it to a
+ * height balanced BST.
+ *
+ * For this problem, a height-balanced binary tree is defined as a binary tree
+ * in which the depth of the two subtrees of every node never differ by more
+ * than 1.
+ *
+ * Example:
+ *
+ *
+ * Given the sorted array: [-10,-3,0,5,9],
+ *
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following
+ * height balanced BST:
+ *
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ *
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildBST(nums []int, s, e int) *TreeNode {
+	if s == e {
+		return nil
+	}
+
+	m := (e + s) / 2
+
+	return &TreeNode{
+		Val:   nums[m],
+		Left:  buildBST(nums, s, m),
+		Right: buildBST(nums, m+1, e),
+	}
+}
+func sortedArrayToBST(nums []int) *TreeNode {
+	return buildBST(nums, 0, len(nums))
+}
