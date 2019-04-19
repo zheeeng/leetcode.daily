e6d3a6f21c643a1ddace75f8d05755006348b8de
Zheeeng <hi@zheeeng.me>
2019-04-19 16:54:03 +0800

    160/go

---
+++ 160.intersection-of-two-linked-lists.0.go
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode id=160 lang=golang
+ *
+ * [160] Intersection of Two Linked Lists
+ *
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+ *
+ * algorithms
+ * Easy (32.92%)
+ * Total Accepted:    291.4K
+ * Total Submissions: 877.4K
+ * Testcase Example:  '8\n[4,1,8,4,5]\n[5,0,1,8,4,5]\n2\n3'
+ *
+ * Write a program to find the node at which the intersection of two singly
+ * linked lists begins.
+ *
+ * For example, the following two linked lists:
+ *
+ *
+ * begin to intersect at node c1.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ *
+ * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA =
+ * 2, skipB = 3
+ * Output: Reference of the node with value = 8
+ * Input Explanation: The intersected node's value is 8 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2
+ * nodes before the intersected node in A; There are 3 nodes before the
+ * intersected node in B.
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ *
+ * Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3,
+ * skipB = 1
+ * Output: Reference of the node with value = 2
+ * Input Explanation: The intersected node's value is 2 (note that this must
+ * not be 0 if the two lists intersect). From the head of A, it reads as
+ * [0,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes
+ * before the intersected node in A; There are 1 node before the intersected
+ * node in B.
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ *
+ * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB =
+ * 2
+ * Output: null
+ * Input Explanation: From the head of A, it reads as [2,6,4]. From the head of
+ * B, it reads as [1,5]. Since the two lists do not intersect, intersectVal
+ * must be 0, while skipA and skipB can be arbitrary values.
+ * Explanation: The two lists do not intersect, so return null.
+ *
+ *
+ *
+ *
+ * Notes:
+ *
+ *
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function
+ * returns.
+ * You may assume there are no cycles anywhere in the entire linked
+ * structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	nodeA := headA
+	nodeB := headB
+
+	count := 0
+	overA := false
+	lA := 1
+	overB := false
+	lB := 1
+
+	for count < lA*lB {
+		if !overA {
+			lA++
+		}
+		if !overB {
+			lB++
+		}
+		count++
+
+		if nodeA == nodeB {
+			return nodeA
+		}
+
+		nodeA = nodeA.Next
+		nodeB = nodeB.Next
+
+		if nodeA == nil {
+			overA = true
+			nodeA = headA
+		}
+		if nodeB == nil {
+			overB = true
+			nodeB = headB
+		}
+	}
+	return nil
+}
