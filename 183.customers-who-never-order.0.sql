e5cc69dc3ac2e6b8d270c0c66e09c5e184e71cc1
Zheeeng <hi@zheeeng.me>
2019-04-20 02:13:21 +0800

    183/sql

---
+++ 183.customers-who-never-order.0.sql
@@ -0,0 +1,54 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+-- https://leetcode.com/problems/customers-who-never-order/description/
+--
+-- database
+-- Easy (43.60%)
+-- Total Accepted:    97.4K
+-- Total Submissions: 221.4K
+-- Testcase Example:  '{"headers": {"Customers": ["Id", "Name"], "Orders": ["Id", "CustomerId"]}, "rows": {"Customers": [[1, "Joe"], [2, "Henry"], [3, "Sam"], [4, "Max"]], "Orders": [[1, 3], [2, 1]]}}'
+--
+-- Suppose that a website contains two tables, the Customers table and the
+-- Orders table. Write a SQL query to find all customers who never order
+-- anything.
+--
+-- Table: Customers.
+--
+--
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+--
+--
+-- Table: Orders.
+--
+--
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+--
+--
+-- Using the above tables as example, return the following:
+--
+--
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+--
+--
+--
+SELECT Name AS Customers FROM Customers WHERE Id NOT IN (SELECT CustomerId as Id FROM Orders)
