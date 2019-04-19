547d8bf474d25c7db452bd1ceb1b2b4b3db90a66
Zheeeng <hi@zheeeng.me>
2019-04-19 15:40:28 +0800

    141/go

---
+++ 141.linked-list-cycle.0.go
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (36.24%)
+ * Total Accepted:    387.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * Can you solve it using O(1) (i.e. constant) memory?
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	head1, head2 := head, head.Next
+	for head1 != head2 {
+		if head2 == nil {
+			return false
+		}
+		head1 = head1.Next
+		head2 = head2.Next
+		if head2 == nil {
+			return false
+		}
+		head2 = head2.Next
+	}
+	return true
+}
