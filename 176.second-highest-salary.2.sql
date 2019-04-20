f9eb988ef787edd9045996014639bafbc533a969
Zheeeng <hi@zheeeng.me>
2019-04-20 16:02:47 +0800

    176/sql-2

---
+++ 176.second-highest-salary.2.sql
@@ -0,0 +1,40 @@
+--
+-- @lc app=leetcode id=176 lang=mysql
+--
+-- [176] Second Highest Salary
+--
+-- https://leetcode.com/problems/second-highest-salary/description/
+--
+-- database
+-- Easy (26.56%)
+-- Total Accepted:    123.2K
+-- Total Submissions: 460.2K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+--
+-- Write a SQL query to get the second highest salary from the Employee
+-- table.
+--
+--
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+--
+--
+-- For example, given the above Employee table, the query should return 200 as
+-- the second highest salary. If there is no second highest salary, then the
+-- query should return null.
+--
+--
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+--
+--
+--
+SELECT (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary
