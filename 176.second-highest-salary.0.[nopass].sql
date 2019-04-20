bdb6bdd7008765e61ac4fade3439277182ea4396
Zheeeng <hi@zheeeng.me>
2019-04-20 14:41:08 +0800

    176/sql-nopass

---
+++ 176.second-highest-salary.0.[nopass].sql
@@ -0,0 +1,41 @@
+--
+-- @lc app=leetcode id=176 lang=mysql
+--
+-- [176] Second Highest Salary
+--
+-- https://leetcode.com/problems/second-highest-salary/description/
+--
+-- database
+-- Easy (26.56%)
+-- Total Accepted:    123.2K
+-- Total Submissions: 460.2K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+--
+-- Write a SQL query to get the second highest salary from the Employee
+-- table.
+--
+--
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+--
+--
+-- For example, given the above Employee table, the query should return 200 as
+-- the second highest salary. If there is no second highest salary, then the
+-- query should return null.
+--
+--
+-- +---------------------+
+-- | SecondHighestSalary |
+-- +---------------------+
+-- | 200                 |
+-- +---------------------+
+--
+--
+--
+# No pass, need to handle empty data situation, returns null
+SELECT Salary AS SecondHighestSalary FROM Employee ORDER BY Salary LIMIT 1 OFFSET 1
