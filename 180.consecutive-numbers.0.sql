31718a07a3f101ab35fc3a4b8366d7d36cba2fe8
Zheeeng <hi@zheeeng.me>
2019-04-20 17:11:16 +0800

    180/sql

---
+++ 180.consecutive-numbers.0.sql
@@ -0,0 +1,48 @@
+--
+-- @lc app=leetcode id=180 lang=mysql
+--
+-- [180] Consecutive Numbers
+--
+-- https://leetcode.com/problems/consecutive-numbers/description/
+--
+-- database
+-- Medium (32.66%)
+-- Total Accepted:    51K
+-- Total Submissions: 154.6K
+-- Testcase Example:  '{"headers": {"Logs": ["Id", "Num"]}, "rows": {"Logs": [[1, 1], [2, 1], [3, 1], [4, 2], [5, 1], [6, 2], [7, 2]]}}'
+--
+-- Write a SQL query to find all numbers that appear at least three times
+-- consecutively.
+--
+--
+-- +----+-----+
+-- | Id | Num |
+-- +----+-----+
+-- | 1  |  1  |
+-- | 2  |  1  |
+-- | 3  |  1  |
+-- | 4  |  2  |
+-- | 5  |  1  |
+-- | 6  |  2  |
+-- | 7  |  2  |
+-- +----+-----+
+--
+--
+-- For example, given the above Logs table, 1 is the only number that appears
+-- consecutively for at least three times.
+--
+--
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+--
+--
+--
+SELECT DISTINCT Num as ConsecutiveNums FROM
+    (SELECT l1.Id, l1.Num, l2.Num as Num2, l3.Num as Num3 FROM Logs l1
+        LEFT JOIN Logs l2 ON l1.Id = l2.Id - 1
+        LEFT JOIN Logs l3 ON l2.Id = l3.Id - 1
+    ) AS tmp
+WHERE tmp.Num = tmp.Num2 AND tmp.Num2 = tmp.Num3
