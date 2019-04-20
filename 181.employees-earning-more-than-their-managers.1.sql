e8852c3c66d6f26117bda3664d73e45bb1c894c9
Zheeeng <hi@zheeeng.me>
2019-04-20 16:29:19 +0800

    opt 181/sql-1

--- 181.employees-earning-more-than-their-managers.1.sql
+++ 181.employees-earning-more-than-their-managers.1.sql
@@ -38,6 +38,6 @@
 --
 --
 --
-SELECT e1.Name as Employee FROM Employee e1 LEFT JOIN Employee e2
+SELECT e1.Name as Employee FROM Employee e1 JOIN Employee e2
     ON e1.ManagerId = e2.Id
     WHERE e1.Salary > e2.Salary
