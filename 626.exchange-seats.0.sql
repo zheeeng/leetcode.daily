cded43b6a9bd51ed3105c2fd033a4297603fd322
Zheeeng <hi@zheeeng.me>
2019-04-20 19:13:49 +0800

    626/sql

---
+++ 626.exchange-seats.0.sql
@@ -0,0 +1,61 @@
+--
+-- @lc app=leetcode id=626 lang=mysql
+--
+-- [626] Exchange Seats
+--
+-- https://leetcode.com/problems/exchange-seats/description/
+--
+-- database
+-- Medium (53.42%)
+-- Total Accepted:    22.2K
+-- Total Submissions: 41.4K
+-- Testcase Example:  '{"headers": {"seat": ["id","student"]}, "rows": {"seat": [[1,"Abbot"],[2,"Doris"],[3,"Emerson"],[4,"Green"],[5,"Jeames"]]}}'
+--
+-- Mary is a teacher in a middle school and she has a table seat storing
+-- students' names and their corresponding seat ids.
+-- The column id is continuous increment.
+--
+--
+-- Mary wants to change seats for the adjacent students.
+--
+--
+-- Can you write a SQL query to output the result for Mary?
+--
+--
+--
+--
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Abbot   |
+-- |    2    | Doris   |
+-- |    3    | Emerson |
+-- |    4    | Green   |
+-- |    5    | Jeames  |
+-- +---------+---------+
+--
+-- For the sample input, the output is:
+--
+--
+--
+--
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Doris   |
+-- |    2    | Abbot   |
+-- |    3    | Green   |
+-- |    4    | Emerson |
+-- |    5    | Jeames  |
+-- +---------+---------+
+--
+--
+-- Note:
+-- If the number of students is odd, there is no need to change the last one's
+-- seat.
+--
+--
+SELECT s2.id, s1.student FROM seat s1 LEFT JOIN seat s2
+    ON (s1.id % 2 = 1 AND s1.id = s2.id - 1)
+    OR (s1.id % 2 = 0 AND s1.id = s2.id + 1)
+    OR (s1.id % 2 = 1 AND s1.id = s2.id AND s1.id = (SELECT max(id) FROM seat))
