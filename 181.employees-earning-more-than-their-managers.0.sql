212abb79c729533ee57e8bc74c04f6a55338550a
Zheeeng <hi@zheeeng.me>
2019-04-20 16:14:51 +0800

    181/sql

---
+++ 181.employees-earning-more-than-their-managers.0.sql
@@ -0,0 +1,44 @@
+--
+-- @lc app=leetcode id=181 lang=mysql
+--
+-- [181] Employees Earning More Than Their Managers
+--
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/description/
+--
+-- database
+-- Easy (47.14%)
+-- Total Accepted:    103.7K
+-- Total Submissions: 218.3K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Name", "Salary", "ManagerId"]}, "rows": {"Employee": [[1, "Joe", 70000, 3], [2, "Henry", 80000, 4], [3, "Sam", 60000, null], [4, "Max", 90000, null]]}}'
+--
+-- The Employee table holds all employees including their managers. Every
+-- employee has an Id, and there is also a column for the manager Id.
+--
+--
+-- +----+-------+--------+-----------+
+-- | Id | Name  | Salary | ManagerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | NULL      |
+-- | 4  | Max   | 90000  | NULL      |
+-- +----+-------+--------+-----------+
+--
+--
+-- Given the Employee table, write a SQL query that finds out employees who
+-- earn more than their managers. For the above table, Joe is the only employee
+-- who earns more than his manager.
+--
+--
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+--
+--
+--
+SELECT Name as Employee FROM
+    (SELECT e1.Name, e1.Salary, e2.Salary as ManagerSalary FROM Employee e1 LEFT JOIN Employee e2
+        ON e1.ManagerId = e2.Id) as TMP
+WHERE TMP.Salary > TMP.ManagerSalary
