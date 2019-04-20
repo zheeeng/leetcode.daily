2a388829e4282f6fc7dd76df6bd3428b532db67e
Zheeeng <hi@zheeeng.me>
2019-04-20 13:26:29 +0800

    182/sql-2

---
+++ 182.duplicate-emails.2.sql
@@ -0,0 +1,40 @@
+--
+-- @lc app=leetcode id=182 lang=mysql
+--
+-- [182] Duplicate Emails
+--
+-- https://leetcode.com/problems/duplicate-emails/description/
+--
+-- database
+-- Easy (53.31%)
+-- Total Accepted:    107.9K
+-- Total Submissions: 200.9K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "a@b.com"], [2, "c@d.com"], [3, "a@b.com"]]}}'
+--
+-- Write a SQL query to find all duplicate emails in a table named Person.
+--
+--
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+--
+--
+-- For example, your query should return the following for the above table:
+--
+--
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+--
+--
+-- Note: All emails are in lowercase.
+--
+--
+SELECT DISTINCT p1.Email from Person p1, Person P2
+    WHERE p1.Id <> p2.Id AND p1.Email = p2.Email
