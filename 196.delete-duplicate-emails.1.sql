64a2f413f250e76fd35bb26c443d219cd610bd43
Zheeeng <hi@zheeeng.me>
2019-04-20 14:34:24 +0800

    196/sql-1

---
+++ 196.delete-duplicate-emails.1.sql
@@ -0,0 +1,49 @@
+--
+-- @lc app=leetcode id=196 lang=mysql
+--
+-- [196] Delete Duplicate Emails
+--
+-- https://leetcode.com/problems/delete-duplicate-emails/description/
+--
+-- database
+-- Easy (31.46%)
+-- Total Accepted:    68K
+-- Total Submissions: 213.4K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "john@example.com"], [2, "bob@example.com"], [3, "john@example.com"]]}}'
+--
+-- Write a SQL query to delete all duplicate email entries in a table named
+-- Person, keeping only unique emails based on its smallest Id.
+--
+--
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+-- Id is the primary key column for this table.
+--
+--
+-- For example, after running your query, the above Person table should have
+-- the following rows:
+--
+--
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+
+--
+--
+-- Note:
+--
+-- Your output is the whole Person table after executing your sql. Use delete
+-- statement.
+--
+--
+DELETE FROM Person WHERE Id NOT IN
+    (SELECT Id FROM
+        (SELECT min(Id) as Id FROM Person GROUP BY Email) AS DistinctPerson
+    )
