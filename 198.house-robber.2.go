abb2f1fb3b6cdabde00f3a540f306bfc2c52f814
Zheeeng <hi@zheeeng.me>
2019-04-21 20:40:35 +0800

    198/go-2

---
+++ 198.house-robber.2.go
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=198 lang=golang
+ *
+ * [198] House Robber
+ *
+ * https://leetcode.com/problems/house-robber/description/
+ *
+ * algorithms
+nc * Easy (40.87%)
+ * Total Accepted:    308.7K
+ * Total Submissions: 754.8K
+ * Testcase Example:  '[1,2,3,1]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed, the only constraint stopping
+ * you from robbing each of them is that adjacent houses have security system
+ * connected and it will automatically contact the police if two adjacent
+ * houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
+ * 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ *
+ * Example 2:
+ *
+ *
+ * Input: [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house
+ * 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ *
+ *
+*/
+func max(a, b int) (r int) {
+	r = a
+	if b > a {
+		r = b
+	}
+	return
+}
+func rob(nums []int) int {
+	e, o := 0, 0
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		if i%2 == 0 {
+			e = max(e+nums[i], o)
+		} else {
+			o = max(e, o+nums[i])
+		}
+	}
+	return max(e, o)
+}
