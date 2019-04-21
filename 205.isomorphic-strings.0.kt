d18747df025e68ed455c22d0de80bfb9fd1407bf
Zheeeng <hi@zheeeng.me>
2019-04-22 02:27:41 +0800

    205/kt

---
+++ 205.isomorphic-strings.0.kt
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=205 lang=kotlin
+ *
+ * [205] Isomorphic Strings
+ *
+ * https://leetcode.com/problems/isomorphic-strings/description/
+ *
+ * algorithms
+ * Easy (36.94%)
+ * Total Accepted:    196.4K
+ * Total Submissions: 530K
+ * Testcase Example:  '"egg"\n"add"'
+ *
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same
+ * character but a character may map to itself.
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "egg", t = "add"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "paper", t = "title"
+ * Output: true
+ *
+ * Note:
+ * You may assume both s and t have the same length.
+ *
+ */
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean =
+        (s zip t).toSet().size.let { it == s.toSet().size && it == t.toSet().size }
+}
