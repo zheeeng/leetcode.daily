64ad401baf40cb3c7fdce1984a1e50314ed878b8
Zheeeng <hi@zheeeng.me>
2019-04-21 10:44:28 +0800

    225/go

---
+++ 225.implement-stack-using-queues.0.go
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=225 lang=golang
+ *
+ * [225] Implement Stack using Queues
+ *
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ *
+ * algorithms
+ * Easy (38.48%)
+ * Total Accepted:    125.4K
+ * Total Submissions: 323.7K
+ * Testcase Example:  '["MyStack","push","push","top","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement the following operations of a stack using queues.
+ *
+ *
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ *
+ *
+ * Example:
+ *
+ *
+ * MyStack stack = new MyStack();
+ *
+ * stack.push(1);
+ * stack.push(2);
+ * stack.top();   // returns 2
+ * stack.pop();   // returns 2
+ * stack.empty(); // returns false
+ *
+ * Notes:
+ *
+ *
+ * You must use only standard operations of a queue -- which means only push to
+ * back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively. You may
+ * simulate a queue by using a list or deque (double-ended queue), as long as
+ * you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example, no pop or top
+ * operations will be called on an empty stack).
+ *
+ *
+ */
+type MyStack struct {
+	s []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x int) {
+	this.s = append(this.s, x)
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() (v int) {
+	this.s, v = this.s[0:len(this.s)-1], this.s[len(this.s)-1]
+	return
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() int {
+	return this.s[len(this.s)-1]
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return len(this.s) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
+
