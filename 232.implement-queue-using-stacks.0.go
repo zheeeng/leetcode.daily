2492bf252d239173bc0b38eff92ffe86ad23b531
Zheeeng <hi@zheeeng.me>
2019-04-23 03:27:35 +0800

    232/go

---
+++ 232.implement-queue-using-stacks.0.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=232 lang=golang
+ *
+ * [232] Implement Queue using Stacks
+ *
+ * https://leetcode.com/problems/implement-queue-using-stacks/description/
+ *
+ * algorithms
+ * Easy (42.54%)
+ * Total Accepted:    144.7K
+ * Total Submissions: 338K
+ * Testcase Example:  '["MyQueue","push","push","peek","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement the following operations of a queue using stacks.
+ *
+ *
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+ *
+ *
+ * Example:
+ *
+ *
+ * MyQueue queue = new MyQueue();
+ *
+ * queue.push(1);
+ * queue.push(2);
+ * queue.peek();  // returns 1
+ * queue.pop();   // returns 1
+ * queue.empty(); // returns false
+ *
+ * Notes:
+ *
+ *
+ * You must use only standard operations of a stack -- which means only push to
+ * top, peek/pop from top, size, and is empty operations are valid.
+ * Depending on your language, stack may not be supported natively. You may
+ * simulate a stack by using a list or deque (double-ended queue), as long as
+ * you use only standard operations of a stack.
+ * You may assume that all operations are valid (for example, no pop or peek
+ * operations will be called on an empty queue).
+ *
+ *
+ */
+type MyQueue struct {
+	q []int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	this.q = append(this.q, x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() (x int) {
+	x, this.q = this.q[0], this.q[1:len(this.q)]
+	return
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	return this.q[0]
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	return len(this.q) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
