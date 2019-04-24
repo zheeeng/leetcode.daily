cf6cc2958ff48990f49a8d61987813d22e7b5f51
Zheeeng <hi@zheeeng.me>
2019-04-25 04:33:31 +0800

    237/go

---
+++ 237.delete-node-in-a-linked-list.0.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=237 lang=golang
+ *
+ * [237] Delete Node in a Linked List
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    node.Val = node.Next.Val
+    node.Next = node.Next.Next
+}
