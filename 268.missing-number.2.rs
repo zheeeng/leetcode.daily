b40df97ad3597dc73ab12eac9f6ccaa96842fe4b
Zheeeng <hi@zheeeng.me>
2019-04-26 12:03:52 +0800

    268/rs-2

---
+++ 268.missing-number.2.rs
@@ -0,0 +1,10 @@
+/*
+ * @lc app=leetcode id=268 lang=rust
+ *
+ * [268] Missing Number
+ */
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        nums.iter().enumerate().fold(0, |s, (i, n) | s ^ i  as i32 ^ n) ^ nums.len() as i32
+    }
+}
