0542e8c150ce2e6a1326a37bc46f62f00fa18fc1
Zheeeng <hi@zheeeng.me>
2019-04-30 19:08:02 +0800

    1010/kt-1

---
+++ 1010.pairs-of-songs-with-total-durations-divisible-by-60.1.kt
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1010 lang=kotlin
+ *
+ * [1010] Pairs of Songs With Total Durations Divisible by 60
+ */
+class Solution {
+    fun combin2s (n: Int): Int {
+        return n * (n - 1) / 2
+    }
+    fun numPairsDivisibleBy60(time: IntArray): Int {
+        return IntArray(61) { _ -> 0 }
+            .also { arr ->
+                time.map { t -> t % 60 }.forEach { v -> arr[v]++ }
+            }
+            .let { arr -> arr.take(30).foldIndexed(combin2s(arr[0]) + combin2s(arr[30])) { i, acc, v ->
+                acc + v * arr[60 - i]
+            }}
+    }
+}
