2503f3cd182bc9d94fb8fd5397f988bb746422a5
Zheeeng <hi@zheeeng.me>
2019-05-07 19:45:53 +0800

    783/go-1-inorder

---
+++ 783.minimum-distance-between-bst-nodes.1.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=783 lang=golang
+ *
+ * [783] Minimum Distance Between BST Nodes
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+import (
+    "sort"
+)
+func minDiffInBST(root *TreeNode) int {
+    vals := []int{root.Val}
+    nodeList := []*TreeNode{root}
+    lastNode := root
+
+    for len(nodeList) > 0 {
+        for lastNode.Left != nil {
+            lastNode = lastNode.Left
+            nodeList = append(nodeList, lastNode)
+            vals = append(vals, lastNode.Val)
+        }
+
+        for len(nodeList) > 0 {
+            lastNode, nodeList = nodeList[len(nodeList)-1], nodeList[:len(nodeList)-1]
+
+            if lastNode.Right != nil {
+                lastNode = lastNode.Right
+                nodeList = append(nodeList, lastNode)
+                vals = append(vals, lastNode.Val)
+                break
+            }
+        }
+    }
+
+    sort.Ints(vals)
+
+    min := vals[1] - vals[0]
+    for i := 2; i < len(vals); i++ {
+        diff := vals[i] - vals[i - 1]
+        if diff < min {
+            min = diff
+        }
+    }
+    return min
+}
