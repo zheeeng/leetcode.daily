a75108536e4341a5670ef586e037941331f46549
Zheeeng <hi@zheeeng.me>
2019-05-14 21:33:34 +0800

    1025/kt

---
+++ 1025.divisor-game.0.kt
@@ -0,0 +1,10 @@
+/*
+ * @lc app=leetcode id=1025 lang=kotlin
+ *
+ * [1025] Divisor Game
+ */
+class Solution_divisorGame_0 {
+    fun divisorGame(N: Int): Boolean {
+        return N % 2 == 0
+    }
+}
