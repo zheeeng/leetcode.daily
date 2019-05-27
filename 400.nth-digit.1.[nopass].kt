8bb3732757a1f2b0f791615dfdd65c9e952095eb
Zheeeng <hi@zheeeng.me>
2019-05-27 09:32:35 +0800

    400/kt-1

---
+++ 400.nth-digit.1.[nopass].kt
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=400 lang=kotlin
+ *
+ * [400] Nth Digit
+ */
+import kotlin.math.pow
+import kotlin.math.ceil
+
+class Solution {
+    fun findNthDigit(n: Int): Int {
+        val (count, max) = generateSequence(Pair(0, 0)) { (count, max) ->
+        	(count + 1).let { Pair(it, max + (it * 10.toDouble().pow(it) * 0.9).toInt()) }.takeIf { it.second < n }
+        }.last().also(::println)
+
+        return (n - max)
+            .let {
+                (10.toDouble().pow(count).toInt() + (it - 1).toDouble() / (count + 1)).toInt()
+                    .toString().also(::println)
+                    .get((it - 1) % (count + 1)).toString().toInt()
+            }
+    }
+}
